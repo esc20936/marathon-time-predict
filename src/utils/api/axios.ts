4dd90f2fc97d0510ee2cc5331cdfd962025b74a8
Pablo Escobar <esc20936@uvg.edu.gt>
2024-11-13 21:20:32 -0600

    feat(): correct endpoint

--- src/utils/api/axios.ts
+++ src/utils/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://c319-18-220-100-215.ngrok-free.app",
+  baseURL: "https://mlops-api.click/",
 });
 
 export default API;
